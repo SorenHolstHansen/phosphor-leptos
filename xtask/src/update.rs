eeaba17bfa14fe49bebb43f327d9d423a0cbee23
Corvus <corvusprudens@gmail.com>
2024-09-18 21:46:32 -0600

    Swapped over to quote for templating

--- xtask/src/update.rs
+++ xtask/src/update.rs
@@ -1,14 +1,16 @@
 use convert_case::{Case, Casing};
+use proc_macro2::TokenStream;
+use quote::{format_ident, quote};
 use regex::Regex;
 use std::collections::{BTreeMap, HashMap};
 use std::{fs, process};
 
-fn match_case(weight_name: &str, svg: &str) -> String {
-    format!(
-        "IconWeight::{} => view!{{ {} }}.into_view()",
-        weight_name.to_case(Case::UpperCamel),
-        svg
-    )
+fn match_case(weight_name: &str, svg: &str) -> TokenStream {
+    let weight_ident = format_ident!("{}", weight_name.to_case(Case::UpperCamel));
+
+    quote! {
+        IconWeight::#weight_ident => view! { #svg }.into_view()
+    }
 }
 
 fn extract_categories(input: &str) -> (HashMap<String, Vec<String>>, BTreeMap<String, ()>) {
@@ -41,6 +43,7 @@ fn extract_categories(input: &str) -> (HashMap<String, Vec<String>>, BTreeMap<St
     categories_set.insert("uncategorized".to_string(), ());
     (icon_categories, categories_set)
 }
+
 fn cargo_template(features: &BTreeMap<String, ()>) -> String {
     let mut template = r#"# GENERATED FILE!
 # Edit xtask/src/update.rs to maintain this file
@@ -81,65 +84,64 @@ default = ["all"]
     ));
 
     // now add the rest, read from the icon_categories
-    for (feature, _) in features {
+    for feature in features.keys() {
         template.push_str(&format!("{feature} = []\n"));
     }
 
     template
 }
 
-fn icon_template(
+fn icon_template<'a>(
     icon_name: &str,
-    icon_weights: Vec<(String, String)>,
-    features: &String,
-) -> String {
-    let component_name = icon_name.to_case(Case::UpperCamel);
-    format!(
-        r#"//! GENERATED FILE
+    icon_weights: impl Iterator<Item = (String, String)>,
+    features: impl Iterator<Item = &'a String>,
+) -> TokenStream {
+    let component_ident = format_ident!("{}", icon_name.to_case(Case::UpperCamel));
 
-use leptos::*;
-use crate::IconWeight;
+    let weights = icon_weights.map(|w| match_case(&w.0, &w.1));
 
-#[cfg(any({features}))]
-#[component]
-pub fn {component_name}(
-    #[prop(into, default = MaybeSignal::Static(IconWeight::Regular))] weight: MaybeSignal<IconWeight>,
-    #[prop(into, default = TextProp::from("1em"))] size: TextProp,
-    #[prop(into, default = TextProp::from("currentColor"))] color: TextProp,
-    #[prop(into, default = MaybeSignal::Static(false))] mirrored: MaybeSignal<bool>,
-    #[prop(into, optional)] id: MaybeProp<TextProp>,
-    #[prop(into, optional)] class: MaybeProp<TextProp>,
-) -> impl IntoView {{
-    let body = Signal::derive(move || {{
-        match weight.get() {{
-            {}
-        }}
-    }});
+    quote! {
+        //! GENERATED FILE
 
-    let transform = move || if mirrored.get() {{ "scale(-1, 1)" }} else {{ "" }};
-    let height = size.clone();
+        use leptos::prelude::*;
+        use crate::IconWeight;
 
-    view! {{
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width=move || size.get()
-            height=move || height.get()
-            fill=color
-            transform=transform
-            viewBox="0 0 256 256"
-            id=move || id.get().map(|id| id.get())
-            class=move || class.get().map(|cls| cls.get())
-        >
-            {{body}}
-        </svg>
-    }}
-}}"#,
-        icon_weights
-            .iter()
-            .map(|w| match_case(&w.0, &w.1))
-            .collect::<Vec<_>>()
-            .join(",\n")
-    )
+        #[cfg(any(#(feature = #features),*))]
+        #[component]
+        pub fn #component_ident(
+            #[prop(into, default = MaybeSignal::Static(IconWeight::Regular))] weight: MaybeSignal<IconWeight>,
+            #[prop(into, default = TextProp::from("1em"))] size: TextProp,
+            #[prop(into, default = TextProp::from("currentColor"))] color: TextProp,
+            #[prop(into, default = MaybeSignal::Static(false))] mirrored: MaybeSignal<bool>,
+            #[prop(into, optional)] id: MaybeProp<TextProp>,
+            #[prop(into, optional)] class: MaybeProp<TextProp>,
+        ) -> impl IntoView {
+            let body = Signal::derive(move || {
+                match weight.get() {
+                    #(#weights),*
+                }
+            });
+
+            let transform = move || mirrored.get().then_some("scale(-1, 1)");
+            let height = size.clone();
+
+            leptos::tachys::svg::svg()
+                .child(body)
+                .attr("xmlns", "http://www.w3.org/2000/svg")
+                .attr("width", move || size.get())
+                .attr("height", move || height.get())
+                .attr("fill", color)
+                .attr("transform", transform)
+                .attr("viewBox", "0 0 256 256")
+                .attr("id", move || id.get().map(|id| id.get()))
+                .class(move || class.get().map(|cls| cls.get()))
+        }
+    }
+}
+
+fn format_file(file: TokenStream) -> String {
+    let parsed: syn::File = syn::parse2(file).expect("Error parsing generated token stream");
+    prettyplease::unparse(&parsed)
 }
 
 const OUTPUT_DIR: &str = "src/icons";
@@ -154,6 +156,8 @@ pub fn run() {
     let (icon_categories, categories_set) =
         extract_categories(fs::read_to_string(TYPESCRIPT_EXPORT_FILE).unwrap().as_str());
 
+    let uncategorized = vec!["uncategorized".into()];
+
     // Clean up the icons folder
     let _ = fs::remove_dir_all(OUTPUT_DIR);
     fs::write("src/lib.rs", "").unwrap();
@@ -164,8 +168,9 @@ pub fn run() {
         .unwrap()
         .map(|entry| entry.unwrap().file_name().into_string().unwrap())
         .collect();
+
     let regular_icons = fs::read_dir(format!("{ASSETS_DIR}/regular")).unwrap();
-    let mut mod_content = String::new();
+    let mut mod_content = Vec::new();
     for entry in regular_icons {
         let entry = entry.unwrap();
         if entry.path().is_file() {
@@ -174,70 +179,64 @@ pub fn run() {
 
             //derive the feature set string for this icon from its mappings.
             //If we haven't been able to match the icon's category, assign in to 'Uncategorized'
-            let features = icon_categories
-                .get(&icon_name)
-                .unwrap_or(&vec!["uncategorized".to_string()])
-                .iter()
-                .map(|category| format!(r#"feature ="{category}""#))
-                .collect::<Vec<_>>();
-            let features = features.join(", ");
+            let features = icon_categories.get(&icon_name).unwrap_or(&uncategorized);
 
-            let icon_weights: Vec<_> = weights
-                .iter()
-                .map(|weight| {
-                    let file_name = if weight == "regular" {
-                        format!("{icon_name}.svg")
-                    } else {
-                        format!("{icon_name}-{weight}.svg")
-                    };
-                    let svg =
-                        fs::read_to_string(format!("{ASSETS_DIR}/{weight}/{file_name}")).unwrap();
-                    let svg = svg_tag_regex.replace(&svg, "");
-                    let svg = svg_closing_tag_regex.replace(&svg, "");
-                    (weight.to_string(), svg.to_string())
-                })
-                .collect();
+            let icon_weights = weights.iter().map(|weight| {
+                let file_name = if weight == "regular" {
+                    format!("{icon_name}.svg")
+                } else {
+                    format!("{icon_name}-{weight}.svg")
+                };
+                let svg = fs::read_to_string(format!("{ASSETS_DIR}/{weight}/{file_name}")).unwrap();
+                let svg = svg_tag_regex.replace(&svg, "");
+                let svg = svg_closing_tag_regex.replace(&svg, "");
+                (weight.to_string(), svg.to_string())
+            });
+
+            let file = icon_template(&icon_name, icon_weights, features.iter());
+
             fs::write(
                 format!("{OUTPUT_DIR}/{}.rs", icon_name.to_case(Case::Snake)),
-                icon_template(&icon_name, icon_weights, &features),
+                format_file(file),
             )
             .unwrap();
 
-            mod_content.push_str(&format!(
-                "#[cfg(any({features}))]\nmod {name};\n#[cfg(any({features}))]\npub use {name}::*;\n",
-                name = icon_name.to_case(Case::Snake)
-            ));
+            let mod_name = format_ident!("{}", icon_name.to_case(Case::Snake));
+            mod_content.push(quote! {
+                #[cfg(any(#(feature = #features),*))]
+                mod #mod_name;
+
+                #[cfg(any(#(feature = #features),*))]
+                pub use #mod_name::*;
+            });
         };
     }
 
-    fs::write(format!("{OUTPUT_DIR}/mod.rs"), mod_content).unwrap();
-    fs::write(
-        "src/lib.rs",
-        format!(
-            r#"mod icons;
-pub use icons::*;
+    let module = format_file(quote! { #(#mod_content)* });
+    fs::write(format!("{OUTPUT_DIR}/mod.rs"), module).unwrap();
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum IconWeight {{
-    {}
-}}
-"#,
-            weights
-                .iter()
-                .map(|w| w.to_case(Case::UpperCamel))
-                .collect::<Vec<_>>()
-                .join(", ")
-        ),
-    )
-    .unwrap();
+    let weight_variants = weights
+        .iter()
+        .map(|w| format_ident!("{}", w.to_case(Case::UpperCamel)));
+    let lib = quote! {
+        mod icons;
+        pub use icons::*;
+
+        #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+        pub enum IconWeight {
+            #(#weight_variants),*
+        }
+    };
+
+    fs::write("src/lib.rs", format_file(lib)).expect("Error writing lib file");
 
     // Write out the newly generated cargo file
     fs::write("Cargo.toml", cargo_template(&categories_set)).unwrap();
 
-    process::Command::new("cargo")
-        .arg("fmt")
-        .status()
-        .expect("Error running cargo fmt");
+    // process::Command::new("cargo")
+    //     .arg("fmt")
+    //     .status()
+    //     .expect("Error running cargo fmt");
     process::Command::new("leptosfmt")
         .arg("src")
         .status()
